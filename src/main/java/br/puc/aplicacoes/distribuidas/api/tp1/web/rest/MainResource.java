e79e6a449458c1941f87582228a796405860af81
Guilherme Braga <gui-gbo@hotmail.com>
2017-11-26 00:14:48 -0200

    correcoes da api

--- src/main/java/br/puc/aplicacoes/distribuidas/api/tp1/web/rest/MainResource.java
+++ src/main/java/br/puc/aplicacoes/distribuidas/api/tp1/web/rest/MainResource.java
@@ -1,10 +1,12 @@
 package br.puc.aplicacoes.distribuidas.api.tp1.web.rest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/")
 public class MainResource {
--- src/main/java/br/puc/aplicacoes/distribuidas/api/tp1/web/rest/TipoContaResource.java
+++ src/main/java/br/puc/aplicacoes/distribuidas/api/tp1/web/rest/TipoContaResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.puc.aplicacoes.distribuidas.api.tp1.dto.TipoContaDTO;
 import br.puc.aplicacoes.distribuidas.api.tp1.service.TipoContaService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/tipo-conta")
 public class TipoContaResource {
